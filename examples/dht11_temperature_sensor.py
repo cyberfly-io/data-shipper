b2c69a38b3d9ad1abcd2939fe431b50b00717ed3
Syed Abuthahir <abu@Syeds-MBP.domain.name>
2022-09-03 20:58:23 +0530

    change pin no for article

--- examples/dht11_temperature_sensor.py
+++ examples/dht11_temperature_sensor.py
@@ -1,7 +1,7 @@
 import adafruit_dht
 import board
 from data_shipper.main import CyberflyDataShipper
-dht = adafruit_dht.DHT11(board.D23)
+dht = adafruit_dht.DHT11(board.D4)
 key_pair = {"publicKey": "d04bbd8f403e583248aa461896bd7518113f89b85c98f3d9596bbfbf30df0bcb",
             "secretKey": "a0ec3175c6c80e60bc8ef18bd7b73a631c507b9f0a42c973036c7f96d21b047a"}
 
@@ -11,10 +11,10 @@ client = CyberflyDataShipper(device_id="d843cbb5-94e9-4cd7-9ea0-c8339b11b440", k
 @client.on_message()
 def do_something(data):
     print(data)
-    data_var = data.get('dataVar') # data variable you define in widget
-    if data_var == "temp":
+    data_var = data.get('dataVar')
+    if data_var == "temp":  # data variable you define in widget
         temperature = dht.temperature
-        client.publish(data.get('response_topic'), {data_var:temperature})
+        client.publish(data.get('response_topic'), {data_var: temperature})
 
 
 while 1:
